0a4c4b70fd96e7d3b33e5ee22806bd60d1043004
dialloconkry <43544103+dialloconkry@users.noreply.github.com>
2019-03-20 10:12:36 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,30 @@
+$(".r").mouseenter(function() {
+   $(".r").hide();  
+});
+$("change").click(function() {
+   $("body").css("color","aqua");  
+});
+$("button").click(function() {
+$(".hade").text("double click image");   
+});
+$(".hide").dblclick(function() {
+   $(".hide").slideUp();  
+});
+$(".hide").dblclick(function() {
+   $(".i").show();  
+});
+$(".r").mouseenter(function() {
+   $(".i").hide();  
+});
+$(".p").click(function() {
+   $(".p").text("Thank you Here's your Treasure");  
+});
+$(".r").mouseenter(function() {
+   $(".r").hide();  
+});
+$(".r").mouseenter(function() {
+   $(".fin").hide();  
+});
+$(".p").dblclick(function() {
+   $(".fin").show();  
+});
